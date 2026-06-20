117373d066791c182718f730827ac28818d38a06
Javier Villarreal <villarrealjavier401@gmail.com>
2026-06-20 18:45:34 -0500

    feat: first commit, hero secction was created

---
+++ src/components/sections/HeroSection.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const HeroSection = () => {
+  return (
+    <div>
+        <div>
+            <p>Hola, soy</p>
+            <h1>Javier Villarreal</h1>
+            <h2>Desarrollador Backend intentando ser Full-Stack</h2>
+        </div>
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <p>Alto, inteligente y apuesto</p>
+        </div>
+    </div>
+  )
+}
